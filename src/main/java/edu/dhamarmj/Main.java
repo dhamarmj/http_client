99ae7baf6fd8b105677721f01e55017750933522
dhamarmj <39349434+dhamarmj@users.noreply.github.com>
2019-01-24 15:22:30 -0400

    Contar lineas totales del HTML, agregar el parametro y header Validacion del URL

--- src/main/java/edu/dhamarmj/Main.java
+++ src/main/java/edu/dhamarmj/Main.java
@@ -15,55 +15,66 @@ import org.jsoup.select.Elements;
 import javax.xml.bind.SchemaOutputResolver;
 import java.io.IOException;
 import java.net.URI;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Introduzca un URL: " );
-//        String uri = scanner.next();
-//        scanner.close();
-
-        //String uri="https://en.wikipedia.org/wiki/World_War_II";
-        String uri="https://www.facebook.com";
-        Document doc = Jsoup.connect(uri).get();
-
-        Elements para = doc.getElementsByTag("p");
-        System.out.println("Number of paragraphs: "+ para.size());
-
-        Elements images = doc.select("img > p");
-        System.out.println("Images inside a paragraph: "+ images.size());
-
-        Elements forms = doc.select("form[method$=post],form[method$=get]");
-        System.out.println("forms with method POST or GET: "+ forms.size());
-
-        Elements input = doc.getElementsByTag("input");
-        for (Element a:
-                input) {
-            System.out.println("Input Type: " + a.attr("type"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca un URL: " );
+        String url = scanner.next();
+        scanner.close();
+        Document doc  = validateUrl(url);
+        if(doc != null){
+            System.out.println(" ");
+            System.out.println("Number of lines: " +  doc.html().split("\n").length);
+            System.out.println(" ");
+            Elements para = doc.getElementsByTag("p");
+            System.out.println("Number of paragraphs: "+ para.size());
+            System.out.println(" ");
+            Elements images = doc.select("img > p");
+            System.out.println("Images inside a paragraph: "+ images.size());
+            System.out.println(" ");
+            Elements forms = doc.select("form[method$=post],form[method$=get]");
+            System.out.println("forms with method POST or GET: "+ forms.size());
+            System.out.println(" ");
+            Elements input = doc.getElementsByTag("input");
+            for (Element a:
+                    input) {
+                System.out.println("Input name: " + a.attr("name") + " Type: " + a.attr("type"));
+            }
+            System.out.println(" ");
+            Elements formPost = doc.select("form[method$=post]");
+            for (Element f:
+                    formPost) {
+                postResponse(f);
+            }
         }
+        else
+            System.out.println("Wrong URL");
+    }
 
-
-        Elements formPost = doc.select("form[method$=post]");
-        for (Element f:
-                formPost) {
-            PostResponse(uri);
+    private static Document validateUrl(String url) throws IOException {
+        Document doc;
+        try{
+            doc = Jsoup.connect(url).get();
+            return doc;
+        }
+        catch (IllegalArgumentException e){
+            return null;
+        }
+        catch(UnknownHostException e){
+            return null;
         }
     }
 
-    private static void PostResponse(String uri) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(uri);
-        CloseableHttpResponse response = httpclient.execute(httpPost);
-        try {
-            System.out.println(response.getStatusLine());
-            HttpEntity entity2 = response.getEntity();
-            response.addHeader("asignatura","practica1" );
-            response.addHeader("matricula","20140047" );
-            EntityUtils.consume(entity2);
-        } finally {
-            response.close();
-        }
+
+    private static void postResponse(Element element) throws IOException {
+        String dir = element.absUrl("action");
+            Document document1 = Jsoup.connect(dir)
+                    .data("asignatura","practica1")
+                    .header("matricula","20140047").post();
+            System.out.println(document1.title());
     }
 }
